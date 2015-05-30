172fc895cff08ca2cea3e703c59d6410f4274174
Matic Leva <vertical3life@gmail.com>
2015-05-30 11:00:45 +0200

    Added Main Menu scene

---
+++ Assets/Scripts/ToggleButtonSprite.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ToggleButtonSprite : MonoBehaviour
+{
+
+
+    private Button button;
+    [SerializeField]
+    private Sprite altSprite;
+    private Sprite orgSprite;
+    private Boolean altState = false;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => { OnClick(); });
+
+        orgSprite = GetComponent<Image>().sprite;
+    }
+
+    void OnClick()
+    {
+        altState = !altState;
+        if (altState)
+            GetComponent<Image>().sprite = altSprite;
+        else
+            GetComponent<Image>().sprite = orgSprite;
+
+    }
+}
