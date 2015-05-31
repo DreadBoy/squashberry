8cff861129b0d8e181b298c0fd95819e592c9c9b
GideonUng <gidcheen@gmail.com>
2015-05-31 11:27:32 +0200

    Added level indicator script

---
+++ Assets/BlenderLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlenderLevel : MonoBehaviour
+{
+	public Texture EmptyTexture;
+	public Texture[] LevelTextures;
+
+	public static List<BlenderLevel> Instaces;
+
+	void Awake()
+	{
+		BlenderLevel.Instaces.Add(this);
+	}
+
+	public static void SetLevel(int level)
+	{
+		Instaces[0].GetComponent<Renderer>().material.SetTexture(0, Instaces[0].LevelTextures[level]);
+	}
+}
