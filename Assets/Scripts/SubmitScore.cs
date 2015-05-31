8bc01ef9815bade00dbc667828bbe4aa6aed137d
GideonUng <gidcheen@gmail.com>
2015-05-31 15:33:05 +0200

    gameover sound

--- Assets/Scripts/SubmitScore.cs
+++ Assets/Scripts/SubmitScore.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
+[RequireComponent(typeof(AudioSource))]
+
 public class SubmitScore : MonoBehaviour {
+
+	//public AudioSource audioSource;
 
     public void PopulateScore()
     {
+		GetComponent<AudioSource>().Play();
         GetComponent<Text>().text = "Score: " + Settings.saveData.Score;
     }
 }
